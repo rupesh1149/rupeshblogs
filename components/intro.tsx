6d478bdd103ab5dc0d700e05751056b9853dde44
rupesh1149 <46110950+rupesh1149@users.noreply.github.com>
2023-09-30 13:54:35 +1000

    Initial blogs setup completed

---
+++ components/intro.tsx
@@ -0,0 +1,11 @@
+const Intro = () => {
+  return (
+    <section className="flex-col md:flex-row flex items-center md:justify-between mt-8 mb-8 md:mb-8">
+      <h1 className="text-2xl md:text-5xl font-bold tracking-tighter leading-tight md:pr-8 ">
+        Rupesh's Blogs.
+      </h1>
+    </section>
+  )
+}
+
+export default Intro
